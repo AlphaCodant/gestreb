6f6cc347d1ac489b6e7480d69709875a86f6bc43
Alpha Codant <alphacodant@gmail.com>
2024-12-16 18:01:06 +0100

    Create print2.js

---
+++ public/js/print2.js
@@ -0,0 +1,11 @@
+
+document.querySelector('#montant').addEventListener('click', function () {
+    let cadre = document.getElementById('cadre_frame2');
+    cadre.style.display='inline-block';
+    let cadre2 = document.getElementById('framedis2');
+    cadre2.style.display='inline-block';
+    let printit = document.getElementById('imprimer2');
+    printit.style.display= "inline-block";
+    let close = document.getElementById('fermer2');
+    close.style.display= "inline-block";
+});
