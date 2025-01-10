752799af3a78c7d12936b22b3987db9b358cefd4
Alpha Codant <alphacodant@gmail.com>
2025-01-10 15:44:12 +0100

    Update map.js

--- public/js/map.js
+++ public/js/map.js
@@ -1275,6 +1275,11 @@ function query() {
                                 })
                             })
                         });
+			let opr_col = {"NSMS":'rgba(8, 192, 80, 0.4)',"CCCO":'rgba(56, 39, 4, 0.1)',
+                            "BALJI":'rgba(199, 154, 1, 0.6)',"GCCI":'rgba(180, 116, 159, 0.6)',"WWA":'rgba(234, 149, 203, 0.7)',
+                            "AK INTERNATIONNAL":'rgba(52, 218, 223, 0.7)',
+                            "STE":'rgba(43, 202, 37, 0.5)',"SNG":'rgba(12, 69, 117, 0.8)',"EMBD":'rgba(89, 223, 183, 0.5)',"BALAJI":'rgba(207, 168, 110, 0.4)',
+                            "BALAJI IMPEX":'rgba(154, 38, 169, 0.6)',"TAUNGYA UGF":'rgba(179, 239, 21, 0.5) '};
                         geojson= new ol.layer.Vector({
                             //title:'dfdfd',
                             //title: '<h5>' + value_crop+' '+ value_param +' '+ value_seas+' '+value_level+'</h5>',
@@ -1284,6 +1289,7 @@ function query() {
                             }),
                             style: function (feature) {
                                 style.getText().setText(feature.get('numero'));
+                                style.getFill().setColor(opr_col[feature.get('partenaire')]);
                                 return style;
                               }
                         });
