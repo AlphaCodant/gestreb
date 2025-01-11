156f6880501dc0b0ea22bfe621d678df8fc3d04b
Alpha Codant <alphacodant@gmail.com>
2025-01-11 22:39:21 +0100

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,6 @@ module.exports = {
   content: ["./views/dashboard_conn.ejs","./views/page_accueil.ejs","./views/connexion.ejs","./views/inscription.ejs",
     "./views/validation.ejs","./views/stats.ejs","./views/admin.ejs",
     "./views/admin_inscrit.ejs","./views/admin_membre.ejs","./views/admin_connecte.ejs","./views/admin_demande.ejs"],
-    
   theme: {
     extend: {},
     
